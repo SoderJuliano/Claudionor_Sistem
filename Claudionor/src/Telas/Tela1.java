f8441c0a5ab76754cc70819432098154e36ccf2f
Juliano Soder <juliano_soder@hotmail.com>
2020-01-23 22:42:17 -0300

    iniciando as telas de UI

--- Claudionor/src/Telas/Tela1.java
+++ Claudionor/src/Telas/Tela1.java
@@ -1,6 +1,8 @@
 package Telas;
 
+import java.awt.GridLayout;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -12,11 +14,12 @@ public class Tela1 extends JFrame{
 
 	JFrame frame = new JFrame();
 	JPanel panel1 = new JPanel();
-	Code entrada;
+	
+	HashMap<String, JLabel> labels = new HashMap();
 	JLabel label = new JLabel();
+	JLabel textoSetEntrada = new JLabel(); 
 	public Tela1(){
 		//iniciando as variáveis
-		this.entrada = new Code();
 		
 		
 		
@@ -25,17 +28,19 @@ public class Tela1 extends JFrame{
 		frame.setSize(600, 600);
 		frame.setTitle("Propriedade do Claudionor");
 		frame.add(panel1);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		
+		labels.put("label1", label);
+		labels.put("label2", textoSetEntrada);
+		
+		panel1.setLayout(new GridLayout());
 		panel1.add(label);
+		panel1.add(textoSetEntrada);
 	}
 	
-	public void setMensagem(String mensagem) {
-		label.setText(mensagem);
-		for(int i=0;i<99999; i++) {
-			System.out.println(".");
-			System.out.println(" .");
-			System.out.println("  .");
-			System.out.println("   .");
-		}
+	public void setMensagem(String mensagem, String label) {
+		((JLabel) labels.get(label)).setText(mensagem);
 	}
 	
 }
--- Claudionor/src/principal/Main.java
+++ Claudionor/src/principal/Main.java
@@ -21,19 +21,17 @@ public class Main {
 		Code e= new Code();
 		DateFormat f = DateFormat.getDateInstance(DateFormat.SHORT);
 	     
-	    tela.setMensagem(e.setValorEntrada(2000.00, f.format(data)));
-	    tela.setMensagem(e.setSaida(300, f.format(data)));
+	    tela.setMensagem(e.setValorEntrada(2000.00, f.format(data)), "label1");
+	    tela.setMensagem(e.setSaida(300, f.format(data)), "label1");
 	    
-	    tela.setMensagem("Total de fundos R$ "+e.getValorTotal("19/01/20"));
-	    System.out.println("Total de fundos R$ "+e.getValorTotal("19/01/20"));
+	    tela.setMensagem("Total de fundos R$ "+e.getValorTotal("19/01/20"), "label1");
 	    
 	   // c.set(Calendar.MONTH, Calendar.MARCH);
 	   // Date data2 = c.getTime();
 	    
 	    e.setValorEntrada(600.0, f.format(data));
 	    
-	    tela.setMensagem("Total de fundos no mês de Janeiro é R$ "+e.getGastosMes("01"));
-	    System.out.println("Total de fundos no mês de Janeiro é R$ "+e.getGastosMes("01"));
+	    tela.setMensagem("Total de fundos no mês de Janeiro é R$ "+e.getGastosMes("01"), "label2");
 	    
 	}
 
